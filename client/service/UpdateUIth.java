34c91b5f63c57d56e78ee9436364b16db28c928e
M150121\student <lbh9204@nate.com>
2018-04-24 16:11:59 +0900

    오류 조금 잡음(사용자 정보 갱신 문제)

--- client/service/UpdateUIth.java
+++ client/service/UpdateUIth.java
@@ -31,18 +31,21 @@ public class UpdateUIth implements Runnable {
 				usepc = new UsePc();
 				usepc.setC_id(cus.getC_id());
 				usepcmodel.updateUI(usepc);
+				System.out.println(usepc.getC_id() + " ID입니다");
+				System.out.println(usepc.getUsecharge()+" 사용요금 입니다.");
+				System.out.println(usepc.getUsetime() + " 사용시간 입니다");
 				int hour = 0;
 				int min = usepc.getUsetime();
 				
-				if(min/60 != 0){
-					hour = min/60;
-					min = min%60;
-				}
+//				if(min/60 != 0){
+//					hour = min/60;
+//					min = min%60;
+//				}
 				
 				
 				uv.tfUseTime.setText(hour + "시간 " + min + "분");
 				uv.tfUseCharge.setText(String.valueOf(usepc.getUsecharge()+100) + " 원");
-				Thread.sleep(6000);							//60초간 대기후 실행
+				Thread.sleep(60000);							//60초간 대기후 실행
 			} catch (SQLException e) {
 				System.out.println("좌석 갱신 스레드가 참조하는 SQL Connection이 종료되었습니다.");
 			}  					
